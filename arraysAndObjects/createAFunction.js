6c4e1699361cd35de49169d629d4a7354cf09080
subbuinti <subbuinti1@gmail.com>
2021-09-04 19:46:39 +0530

    Write a function 'getNationalBird' to return the national bird of India.

---
+++ arraysAndObjects/createAFunction.js
@@ -0,0 +1,10 @@
+/*
+ * Write your code here and return the output.
+ */
+function getNationalBird() {
+    let bird = "Peacock";
+    return bird;
+}
+
+let nationalBird = getNationalBird();
+console.log(nationalBird);
