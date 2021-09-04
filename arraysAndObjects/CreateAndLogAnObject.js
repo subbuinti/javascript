b5d4abbbc5cd19427b09087694187d45ee759c38
subbuinti <subbuinti1@gmail.com>
2021-09-04 19:49:19 +0530

    Write a JS program to log a vehicle object with the given properties (key-value pairs).

---
+++ arraysAndObjects/CreateAndLogAnObject.js
@@ -0,0 +1,11 @@
+/*
+ * Write your code here and log the output.
+ */
+let vehicle = {
+    name: 'Mercedes Benz',
+    model: 'C-Class',
+    manufacturer: 'Daimler AG company',
+    length: '4686mm',
+    'cargo capacity': '12.6 cubic feet'
+};
+console.log(vehicle);
