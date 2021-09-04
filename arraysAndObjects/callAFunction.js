ec92d929b9cb7fd14f7ef62333a1dc371d98f7ef
subbuinti <subbuinti1@gmail.com>
2021-09-04 19:45:31 +0530

    Write a JS code to call the given function and log it.

---
+++ arraysAndObjects/callAFunction.js
@@ -0,0 +1,10 @@
+// function greet
+function greet() {
+    return "Hello! Have a nice day";
+  }
+  
+  /*
+   * Write your code here and log the output.
+   */
+  let greetings = greet();
+  console.log(greetings);
