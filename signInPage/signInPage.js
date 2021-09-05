d4a7a7cb26a06391ebc52dbed9d266c1bda2501a
subbuinti <subbuinti1@gmail.com>
2021-09-05 14:07:54 +0530

    added

---
+++ signInPage/signInPage.js
@@ -0,0 +1,7 @@
+let inputElement = document.getElementById("inputElement");
+let signInTextElement = document.getElementById("signInText");
+function signIn() {
+  let inputValue = inputElement.value;
+  let verifyText = "Hi " + inputValue + ", verifying your account...";
+  signInTextElement.textContent = verifyText;
+}
