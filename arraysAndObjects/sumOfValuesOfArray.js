ebd85d03db173ff3a3b870285eb4ec85cf599180
subbuinti <subbuinti1@gmail.com>
2021-09-06 19:35:24 +0530

    Write a JS program to log the sum of the values of an array.

---
+++ arraysAndObjects/sumOfValuesOfArray.js
@@ -0,0 +1,40 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // myArray
+  let myArray = JSON.parse(readLine().replace(/'/g, '"'));
+
+  /*
+   *Write your code here and log the output.
+   */
+   let sumOfValuesInArray =0;
+   
+   for (let value of myArray) {
+       sumOfValuesInArray = sumOfValuesInArray + value;
+   }
+   
+   console.log(sumOfValuesInArray);
+}
