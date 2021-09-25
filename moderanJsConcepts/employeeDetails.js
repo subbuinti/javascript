261f8bdfa139155baffa28a95ee20f3c4534bd08
subbuinti <subbuinti1@gmail.com>
2021-09-25 18:46:51 +0530

    Given two values (name and role) of an employee, write a JS arrow function to return an employee object. Use the keys as mentioned in the sample outputs

---
+++ moderanJsConcepts/employeeDetails.js
@@ -0,0 +1,38 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let name = readLine();
+  let role = readLine();
+
+  /* Please do not modify anything above this line */
+
+  /*
+   * Write the arrow function here and log the output.
+   */
+   let getEmployeeDetailsObject = (name, role) => ({ name, role});
+   let employee = getEmployeeDetailsObject(name, role);
+   console.log(employee);
+}
